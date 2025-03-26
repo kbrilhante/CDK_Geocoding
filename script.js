7eaaae4b2c90630c1b0530a93a5240b6bae54dbe
Karina <kbrilhante@gmail.com>
2025-03-26 14:22:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-// let map;
-// let coord = [-22.9110137, -43.2093727];
-// searchLocation(coord);
-
-
-// function searchLocation(c) {
-//     // const openWeatherKey = "f4f8a8841a8bb6f1dfdf8bc15c1087b0";
-
-//     // // "http://api.openweathermap.org/geo/1.0/direct"
-//     // // "?"
-//     // // "q={city name},{state code},{country code}"
-//     // // "&limit={limit}"
-//     // // "&appid={API key}"
-
-
-//     try {
-//         let opt = {
-//             center: c,
-//             zoom: 13,
-//         }
-//         map = L.map('map', opt);
-//     } catch (error) {
-//         map.setView(c)
-//         console.log(map);
-//     }
-
-
-//     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//         maxZoom: 19,
-//         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-//     }).addTo(map);
-// }
-
 let map;
 const txtLocation = document.getElementById("txtLocation");
 txtLocation.addEventListener('keydown', (e) => {
@@ -40,7 +7,14 @@ txtLocation.addEventListener('keydown', (e) => {
 function searchLocation() {
     const location = txtLocation.value;
     if (location) {
-        
+        getLocation(location).then((data) => {
+            txtLocation.value = data.name + ",";
+            txtLocation.value += data.state ? data.state + "," : ""
+            txtLocation.value += data.country;
+
+            let coord = [data.lat, data.lon];
+            setMap(coord)
+        });
     }
 }
 
@@ -57,7 +31,19 @@ async function getLocation(location) {
     return data;
 }
 
-function aaaa() {
-    txtLocation.value = "Rio de Janeiro";
-    searchLocation();
+function setMap(coord) {
+    try {
+        map = L.map('map', {
+            center: coord,
+            zoom: 13,
+        });
+    } catch (error) {
+        map.setView(coord)
+        console.log(map);
+    }
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 }
