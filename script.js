c4f65d80924d5003d368bb73b19f62421acb431e
Karina <kbrilhante@gmail.com>
2025-03-26 13:35:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-// const openWeatherKey = f4f8a8841a8bb6f1dfdf8bc15c1087b0
+let map;
+let coord = [-22.9110137, -43.2093727];
+searchLocation(coord);
 
-const coord = {
-    lat: -22.9110137,
-    lon: -43.2093727,
+
+function searchLocation(c) {
+    // const openWeatherKey = "f4f8a8841a8bb6f1dfdf8bc15c1087b0";
+    
+    // // "http://api.openweathermap.org/geo/1.0/direct"
+    // // "?"
+    // // "q={city name},{state code},{country code}"
+    // // "&limit={limit}"
+    // // "&appid={API key}"
+    
+    
+    try {
+        let opt = {
+            center: c,
+            zoom: 13,
+        }
+        map = L.map('map', opt);
+    } catch (error) {
+        map.setView(c)
+        console.log(map);
+    }
+    
+    
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 }
-
-// // "http://api.openweathermap.org/geo/1.0/direct"
-// // "?"
-// // "q={city name},{state code},{country code}"
-// // "&limit={limit}"
-// // "&appid={API key}"
-
-let opt = {
-    center: [-22.9110137, -43.2093727],
-    zoom: 13,
-}
-
-var map = L.map('map', opt);
-// var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
