d6f73b065efe459d4719c3a036ff2f396a56e405
Karina <kbrilhante@gmail.com>
2025-03-26 13:45:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
+// let map;
+// let coord = [-22.9110137, -43.2093727];
+// searchLocation(coord);
+
+
+// function searchLocation(c) {
+//     // const openWeatherKey = "f4f8a8841a8bb6f1dfdf8bc15c1087b0";
+    
+//     // // "http://api.openweathermap.org/geo/1.0/direct"
+//     // // "?"
+//     // // "q={city name},{state code},{country code}"
+//     // // "&limit={limit}"
+//     // // "&appid={API key}"
+    
+    
+//     try {
+//         let opt = {
+//             center: c,
+//             zoom: 13,
+//         }
+//         map = L.map('map', opt);
+//     } catch (error) {
+//         map.setView(c)
+//         console.log(map);
+//     }
+    
+    
+//     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         maxZoom: 19,
+//         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+//     }).addTo(map);
+// }
+
 let map;
-let coord = [-22.9110137, -43.2093727];
-searchLocation(coord);
+const txtLocation = document.getElementById("txtLocation");
+txtLocation.addEventListener('keydown', (e) => {
+    if (e.code == "Enter") searchLocation();
+});
 
-
-function searchLocation(c) {
-    // const openWeatherKey = "f4f8a8841a8bb6f1dfdf8bc15c1087b0";
-    
-    // // "http://api.openweathermap.org/geo/1.0/direct"
-    // // "?"
-    // // "q={city name},{state code},{country code}"
-    // // "&limit={limit}"
-    // // "&appid={API key}"
-    
-    
-    try {
-        let opt = {
-            center: c,
-            zoom: 13,
-        }
-        map = L.map('map', opt);
-    } catch (error) {
-        map.setView(c)
-        console.log(map);
-    }
-    
-    
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+function searchLocation() {
+    const location = txtLocation.value;
+    if (location) console.log(location);
 }
